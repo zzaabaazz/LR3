164a4074106071a30c05aca7a0b98392b22ea58f
stud034163 <stud034163@HOMENET75>
2023-10-24 12:12:21 +0300

    Пример(в разработке)

--- LR3/Form1.Designer.cs
+++ LR3/Form1.Designer.cs
@@ -28,20 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.XnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.YnumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.DelButton = new System.Windows.Forms.Button();
+            this.SortButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.XnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YnumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // XnumericUpDown
+            // 
+            this.XnumericUpDown.DecimalPlaces = 1;
+            this.XnumericUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.XnumericUpDown.Location = new System.Drawing.Point(59, 52);
+            this.XnumericUpDown.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.XnumericUpDown.Name = "XnumericUpDown";
+            this.XnumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.XnumericUpDown.TabIndex = 0;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(200, 65);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(59, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // YnumericUpDown
+            // 
+            this.YnumericUpDown.DecimalPlaces = 1;
+            this.YnumericUpDown.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.YnumericUpDown.Location = new System.Drawing.Point(59, 78);
+            this.YnumericUpDown.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.YnumericUpDown.Name = "YnumericUpDown";
+            this.YnumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.YnumericUpDown.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView2.Location = new System.Drawing.Point(344, 133);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // DelButton
+            // 
+            this.DelButton.Location = new System.Drawing.Point(103, 320);
+            this.DelButton.Name = "DelButton";
+            this.DelButton.Size = new System.Drawing.Size(75, 23);
+            this.DelButton.TabIndex = 6;
+            this.DelButton.Text = "button1";
+            this.DelButton.UseVisualStyleBackColor = true;
+            // 
+            // SortButton
+            // 
+            this.SortButton.Location = new System.Drawing.Point(456, 320);
+            this.SortButton.Name = "SortButton";
+            this.SortButton.Size = new System.Drawing.Size(75, 23);
+            this.SortButton.TabIndex = 7;
+            this.SortButton.Text = "button1";
+            this.SortButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SortButton);
+            this.Controls.Add(this.DelButton);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.YnumericUpDown);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.XnumericUpDown);
             this.Name = "Form1";
             this.Text = "Список";
+            ((System.ComponentModel.ISupportInitialize)(this.XnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YnumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown XnumericUpDown;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown YnumericUpDown;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button DelButton;
+        private System.Windows.Forms.Button SortButton;
     }
 }
 
